157e260c7d486fc515cc6f1e3cfd4142323e23a0
E Shaw <shaweric01@gmail.com>
2015-11-09 23:56:25 +0800

    new multiple dicetable

---
+++ multiple-dice-class.py
@@ -0,0 +1,86 @@
+from dicetable import *
+
+
+class MultipleDiceTable(dicetable):
+    
+    def __init__(self):
+        
+        self.table = {0:1}
+        self.dsize_table = {} 
+        
+    def getTableVal(self,value):
+        return self.table.get(value,0)
+    def getTable(self):
+        return self.table
+    def getDsize(self):
+        return self.dsize_table
+    def getTotalDice(self):
+        '''returns how many dice were used to generate the values in your table'''
+        total_dice = 0
+        for dice_num in self.dsize_table.values():
+            total_dice += dice_num
+        return total_dice
+    def max(self):
+        '''returns the highest die roll in table'''
+        out = 0
+        for dsize in self.dsize_table.keys():
+            out += dsize*self.dsize_table[dsize]
+        return out
+    def min(self):
+        '''returns the lowest die roll in table'''
+        total_dice = 0
+        for dice_num in self.dsize_table.values():
+            total_dice += dice_num
+        return total_dice
+    def totalcombos(self):
+        '''returns the total possible number of combinations from a dice table'''
+        out = 1
+        for dsize in self.dsize_table.keys():
+            out *= dsize**self.dsize_table[dsize]
+        return out
+    def highest_frequency(self):
+        '''returns die roll that has the number of rolls (arbitrarily picks one of two equal ones)'''
+        hf_index = 0
+        hf_value = 0        
+        for roll_value in self.table:
+            if self.table[roll_value]>hf_value:
+                hf_value = self.table[roll_value]
+                hf_index = roll_value
+        return hf_index
+            
+    
+    def addADie(self,dsize):
+        '''takes your dicetable object, and calculates all the new combinations
+        if you add a new die of the dsize of the object'''
+        #so for d3, this would take {0:1,1:2} and 
+        #update to {1:1,2:1,3:1}+{2:2,3:2,4:2}
+        self.dsize_table[dsize] = self.dsize_table.get(dsize,0) + 1
+        newdic = {}
+        for el in self.table:
+            currentval = self.table[el]
+            for x in range(dsize):
+                
+                newdic[el+x+1] = (newdic.get(el+x+1,0)+currentval)
+        self.table= newdic
+     
+    def __str__(self):
+        out = ''
+        for dsize in self.dsize_table:
+            out = out+str(self.dsize_table[dsize])+'D'+str(dsize)+'\n'
+        return out    
+    def printTable(self):
+        '''go on. give it a try.  you'll never guess what this function does.
+        it's a surprise'''
+        for el in range(self.min(), self.max()+1):
+            if el<10:
+                print ' '+str(el)+':'+str(self.table[el])
+            else:
+                print(str(el)+':'+str(self.table[el]))  
+                
+    def mean(self):
+        '''i mean, don't you just sometimes look at a table of values
+        and wonder what the mean is?'''
+        mean = 0
+        for dsize in self.dsize_table:
+            mean += self.dsize_table[dsize]*(dsize+1)/2.0 
+        return mean
