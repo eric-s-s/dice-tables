14515d352b530e0507d3b0889ff1b4c0acb58e6a
E Shaw <shaweric01@gmail.com>
2015-11-14 03:50:14 +0800

    re-wrote dicetable and first run with linter

--- multiple_dice_class.py
+++ multiple_dice_class.py
@@ -1,80 +1,100 @@
-class dicetable(object):
-    def __init__(self,dsize):
-        self.totaldice = 0
-        self.table = {0:1}
-        self.dsize = dsize
-        
+#TODO
+#10**30 should be very safe overflow cutoff
+#getvalue changes to getting a tuple,a list of tuples in a range, or all the tuples
 
-    def getTableVal(self,value):
-        return self.table.get(value,0)
-    def getTable(self):
-        return self.table
-    def getDsize(self):
-        return self.dsize
-    def getTotalDice(self):
+class DiceTable(object):
+    def __init__(self, dsize):
+        self._totaldice = 0
+        self._table = {0:1}
+        self._dsize = dsize
+
+
+    def dice_size(self):
+        return self._dsize
+    def number_of_dice(self):
         '''returns how many dice were used to generate the values in your table'''
-        return self.totaldice
-    def max(self):
-        '''returns the highest die roll in table'''
-        return self.dsize*self.totaldice
-    def min(self):
-        '''returns the lowest die roll in table'''
-        return self.totaldice
-    def totalcombos(self):
+        return self._totaldice
+    def total_combinations(self):
         '''returns the total possible number of combinations from a dice table'''
-        return self.dsize**self.totaldice
-    def highest_frequency(self):
-        '''returns die roll that has the number of rolls (arbitrarily picks one of two equal ones)'''
-        return int(self.mean())
-    
-    def addADie(self):
+        return self._dsize**self._totaldice
+
+    def roll_range_top(self):
+        '''returns the highest die roll in table'''
+        return self._dsize*self._totaldice
+    def roll_range_bottom(self):
+        '''returns the lowest die roll in table'''
+        return self._totaldice
+    def roll_range(self):
+        return (self.roll_range_bottom(), self.roll_range_top())
+
+    def roll_frequency(self, roll):
+        '''returns tuple of the roll and it's frequency '''
+        return (roll, self._table.get(roll))
+    def roll_frequency_range(self, start, stopbefore):
+        '''returns a list of tuples (roll,frequency).
+        like regular range function, it stops before envalue'''
+        tuple_list = []
+        for roll in range(start, stopbefore):
+            tuple_list.append((roll, self._table.get(roll)))
+        return tuple_list
+    def roll_frequency_all(self):
+        '''returns a list of tuples IN ORDER for all rolls in table'''
+        start = self.roll_range_bottom()
+        stop = self.roll_range_top()+1
+        return self.roll_frequency_range(start, stop)
+    def highest_frequency_roll(self):
+        '''returns die roll that has the number of rolls 
+        (arbitrarily picks one of two equal ones)'''
+        roll = int(self.mean())
+        return (roll, self._table[roll])
+
+
+    def add_a_die(self):
         '''takes your dicetable object, and calculates all the new combinations
         if you add a new die of the dsize of the object'''
-        #so for d3, this would take {0:1,1:2} and 
+        #so for d3, this would take {0:1,1:2} and
         #update to {1:1,2:1,3:1}+{2:2,3:2,4:2}
-        self.totaldice+=1
+        self._totaldice += 1
         newdic = {}
-        for el in self.table:
-            currentval = self.table[el]
-            for x in range(self.dsize):
-                
-                newdic[el+x+1] = (newdic.get(el+x+1,0)+currentval)
-        self.table= newdic
-    def generator(self,num_dice):
-        for x in range (num_dice):
-            self.addADie()
-        #return self
-        
-         
-          
-            
+        for roll in self._table:
+            currentval = self._table[roll]
+            for die_value in range(1, self._dsize+1):
+
+                newdic[roll+die_value] = \
+                (newdic.get(roll+die_value, 0)+currentval)
+        self._table = newdic
+    def add_many_dice(self, num_dice):
+        for x in range(num_dice):
+            self.add_a_die()
+
+
     def __str__(self):
-        return str(self.totaldice)+'D'+str(self.dsize)    
-  
-                
+        return str(self._totaldice)+'D'+str(self._dsize)
+
+
     def mean(self):
         '''i mean, don't you just sometimes look at a table of values
         and wonder what the mean is?'''
-        return (self.totaldice*(1+self.dsize)/2.0) 
+        return self._totaldice*(1+self._dsize)/2.0 
     def stddev(self):
         '''wassamatter you! you don't know what standard deviation is?'''
         try:
             avg = self.mean()
             sqs = 0
             count = 0
-            for x in self.table.keys():
-                sqs += self.table[x]*((avg - x)**2)
-                count += self.table[x]
-            return round((sqs/count)**0.5,4)
+            for roll in self._table.keys():
+                sqs += self._table[roll]*((avg - roll)**2)
+                count += self._table[roll]
+            return round((sqs/count)**0.5, 4)
         except OverflowError:
             print 'this standard deviation is a gross approximation '+\
             'as i had to int() all the sqrt, because the numbers are just too big'
             avg = self.mean()
             sqs = 0
             count = 0
-            for x in self.table.keys():
-                sqs += self.table[x]*int((avg - x)**2)
-                count += self.table[x]
+            for x in self._table.keys():
+                sqs += self._table[x]*int((avg - x)**2)
+                count += self._table[x]
             return round((sqs/count)**0.5,4)
         
     
@@ -84,7 +104,7 @@ class dicetable(object):
 
 
 
-class MultipleDiceTable(dicetable):
+class MultipleDiceTable(DiceTable):
     
     def __init__(self):
         
@@ -164,9 +184,9 @@ class MultipleDiceTable(dicetable):
         
 
     
-class WeightedDiceTable(dicetable):
+class WeightedDiceTable(DiceTable):
     def __init__(self,lst):
-        dicetable.__init__(self,len(lst))
+        DiceTable.__init__(self,len(lst))
         def _transform_weights(lst):
             #transforms user's lst to a uniform weight table
             total = len(lst)
