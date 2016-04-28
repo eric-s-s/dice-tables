ba39f14659c46492f15047f8e046c0ed2c15b66d
E Shaw <shaweric01@gmail.com>
2016-04-29 03:30:41 +0800

    b4 bed.  rst was a mistake. sticking with readme.md

--- dicetables/__init__.py
+++ dicetables/__init__.py
@@ -1,5 +1,12 @@
 '''All major classes for dicetables can be improted directly as dicetables.Class
-or from original module as dicetables.module.Class.  '''
+or from original module as dicetables.module.Class.
+DiceTable() is a LongIntTable with a list of dice added by method add_die()
+  to see all methods, help(DiceTable) of note,
+        stddev, mean, total_frequency
+4 dice, Die, ModDie, WeightedDie, ModWeightedDie.
+functions for getting info from tables:
+    full_table_string, graph_pts, graph_pts_overflow, stats, ascii_graph
+'''
 
 import dicetables.dicestats
 import dicetables.longintmath
